c94118c655ada9763549fe79cdf33f5a8f58a9cf
Ahmed Zeb <ahmed.z@pacsquare.com>
2023-10-27 15:48:48 +0500

    Add project files.

---
+++ SubjectObserver/IObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern.SubjectClass.Subject
+{
+    public interface IObserver
+    {
+        // Receive Notification from Subject
+        void Update(string availability);
+    }
+}
---
+++ SubjectObserver/ISubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern.SubjectClass.Subject
+{
+    public interface ISubject
+    {
+        void RegisterObserver(IObserver observer);
+
+        // Remove or unregister an observer from the subject.
+        void RemoveObserver(IObserver observer);
+
+        // Notify all registered observers when the state of the subject is changed.
+        void NotifyObservers();
+    }
+}
